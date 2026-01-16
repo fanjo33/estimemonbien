6756ce147f1c30a4e301e80c1d9eb39f962e97fa
fanjo33 <joe33240@gmail.com>
2026-01-16 12:54:51 +0100

    Create initial App component with welcome message

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>EstimeMonBien</h1>
+      <p>
+        Bienvenue 👋<br />
+        L’application fonctionne.
+      </p>
+    </div>
+  );
+}
